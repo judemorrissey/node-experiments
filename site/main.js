f4d397610a391db2fa08fa71cfaf62f8c5135933
Jude Morrissey <juderiffic@yahoo.com>
2016-02-03 20:35:06 -0800

    add 404 and 500 handlers

--- site/main.js
+++ site/main.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.end('HELLO');
 });
 
+app.use((req, res, next) => {
+    res.status(404).end('There ain\'t nothin\' here.');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).end('I don\'t feel so good :x');
+});
+
 app.listen(port, () => {
     log.info('started on port', port);
 });
