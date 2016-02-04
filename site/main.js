5334029ff953ce5ba15a78b4e948abacf85c04fd
Jude Morrissey <juderiffic@yahoo.com>
2016-02-03 23:02:50 -0800

    bunyan > log, minimist cli args, static file serving

--- site/main.js
+++ site/main.js
@@ -1,23 +1,69 @@
 'use strict';
 
-var express = require('express');
+/*******************************************************************
+    Requires
+*******************************************************************/
 
-var log = require('../lib/log');
+var express = require('express'); // HTTP interface
+var bunyan = require('bunyan'); // logging
+var minimist = require('minimist'); // command line arg parsing
 
-var app = express();
+/*******************************************************************
+    Initialization
+*******************************************************************/
+
+var log = bunyan.createLogger({name: 'thisAwesomeApp'});
 var port = 8000;
-app.get('/', (req, res) => {
-    res.end('HELLO');
+
+var args = minimist(process.argv.slice(2));
+Object.keys(args).forEach(key => {
+    if (key === '_') {
+        return;
+    }
+
+    switch (key) {
+    case 'd':
+    case 'debug':
+        log.level(bunyan.DEBUG);
+        break;
+    case 'p':
+    case 'port':
+        port = args[key];
+        break;
+    default:
+        log.debug('ignoring unrecognized command line arg', key + '=' + args[key]);
+    }
 });
 
+log.debug('Debug mode activated.');
+
+/*******************************************************************
+    HTTP Interface
+*******************************************************************/
+
+var app = express();
+
 app.use((req, res, next) => {
-    res.status(404).end('There ain\'t nothin\' here.');
+    log.debug('handling request:', req.url);
+    return next();
+});
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+    return res.end('HELLO');
+});
+
+
+// be certain to have these middleware towards the end of this file so they get ran last in the chain
+app.use((req, res, next) => {
+    return res.status(404).end('There ain\'t nothin\' here.');
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).end('I don\'t feel so good :x');
+    return res.status(500).end('I don\'t feel so good :x');
 });
 
 app.listen(port, () => {
-    log.info('started on port', port);
+    log.info('app started on port', port);
 });
