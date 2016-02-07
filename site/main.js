371c3b6a89cd23da6a37544116fa9410a6c9a6d1
Jude Morrissey <juderiffic@yahoo.com>
2016-02-06 23:29:31 -0800

    a litte bit more feathers experimentation

--- site/main.js
+++ site/main.js
@@ -4,13 +4,22 @@
     Requires
 *******************************************************************/
 
-var feathers = require('feathers'); // Express.js wrapper
 var bunyan = require('bunyan'); // logging
+var body_parser = require('body-parser'); // JSON body parsing
+var feathers = require('feathers'); // Express.js wrapper
+var feathers_nedb = require('feathers-nedb'); // feathers service for nedb
 var minimist = require('minimist'); // command line arg parsing
+var nedb = require('nedb'); // embedded persistent or in-memory database
+var path = require('path'); // path utils
 
 /*******************************************************************
     Initialization
 *******************************************************************/
+
+var db = new nedb({
+    filename: './db-data/todos.db',
+    autoload: true
+});
 
 var log = bunyan.createLogger({name: 'thisAwesomeApp'});
 var port = 8000;
@@ -41,19 +50,30 @@ log.debug('Debug mode activated.');
     HTTP Interface
 *******************************************************************/
 
-var app = feathers();
+var app = feathers()
+    .configure(feathers.rest())
+    .configure(feathers.socketio())
+    .use(body_parser.json())
+    .use(body_parser.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
-    log.debug('handling request:', req.url);
+    log.debug('handling request:', req.method, req.url, req.body);
     return next();
 });
 
-app.use('/static', feathers.static(__dirname + '/static'));
+app.use('/todos', feathers_nedb({
+    Model: db,
+    paginate: {
+        default: 2,
+        max: 4
+    }
+}));
+
+app.use('/static', feathers.static(path.join(__dirname + '/static')));
 
 app.get('/', (req, res) => {
-    return res.end('HELLO');
+    return res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 
 // be certain to have these middleware towards the end of this file so they get ran last in the chain
 app.use((req, res, next) => {
@@ -61,9 +81,11 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    return res.status(500).end('I don\'t feel so good :x');
+    log.error(err);
+    return res.status(err.code ? err.code : 500).end();
 });
 
+// actually start the server
 app.listen(port, () => {
     log.info('app started on port', port);
 });
